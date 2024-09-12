7a3ad06bb9fba8389ef39daee1d6441e3861a14b
Gmvz3090 <franekmalko@gmail.com>
2024-09-12 20:11:19 +0200

    v1.0
    :100:  Featured tools :
    - Katana
    - Subfinder
    - Httpx
    - Sqlmap

---
+++ levant.bat
@@ -0,0 +1,67 @@
+@echo off
+title Levant by @Gmvz3090
+cd files
+mode 100,30
+
+chcp 65001
+cls
+call :menu
+:menu
+color 3
+echo.
+echo.
+call :banner
+echo           ╔═════Levant══════╗
+echo           ║                 ║
+echo           ╠═ 1 katana       ║
+echo           ╠═ 2 subfinder    ║
+echo           ╠═ 3 httpx        ║
+echo           ╠═ 4 sqlmap       ║
+echo           ║                 ║
+echo           ╠═════════════════╝
+echo           ║
+set /p input=.%BS%         ╚════^> 
+echo.
+cls
+color 3
+echo.
+echo.
+call :banner
+echo           ╔═════Levant══════╗
+echo           ║                 ║
+echo           ╠═ 1 katana       ║
+echo           ╠═ 2 subfinder    ║
+echo           ╠═ 3 httpx        ║
+echo           ╠═ 4 sqlmap       ║
+echo           ║                 ║
+echo           ╠═════════════════╝
+echo           ║
+echo           ╠════^> %input%
+echo           ║
+set /p conf=.%BS%         ╚══════^> Config : 
+if /I %input% EQU 1 katana %conf%
+if /I %input% EQU 2 subfinder %conf%
+if /I %input% EQU 3 httpx %conf%
+if /I %input% EQU 4 py sqlmap/sqlmap.py %conf%
+echo.
+echo.
+echo.
+pause
+cls
+call :menu
+
+:banner
+echo.
+echo       ███▓    ▓█████ ██▒   █▓ ▄▄▄       ███▄    █ ▄▄▄█████▓
+echo       ▓██▒    ▓█   ▀▓██░   █▒▒████▄     ██ ▀█   █ ▓  ██▒ ▓▒
+echo       ▒██░    ▒███   ▓██  █▒░▒██  ▀█▄  ▓██  ▀█ ██▒▒ ▓██░ ▒░
+echo       ▒██░    ▒▓█  ▄  ▒██ █░░░██▄▄▄▄██ ▓██▒  ▐▌██▒░ ▓██▓ ░ 
+echo       ░██████▒░▒████▒  ▒▀█░   ▓█   ▓██▒▒██░   ▓██░  ▒██▒ ░ 
+echo       ░ ▒░▓  ░░░ ▒░ ░  ░ ▐░   ▒▒   ▓▒█░░ ▒░   ▒ ▒   ▒ ░░   
+echo        ░ ░ ▒  ░ ░ ░  ░  ░ ░░    ▒   ▒▒ ░░ ░░   ░ ▒░    ░    
+echo        ░ ░      ░       ░░    ░   ▒      ░   ░ ░   ░      
+echo          ░  ░   ░  ░     ░        ░  ░         ░          
+echo                   ░                                
+echo                                                by @Gmvz3090 
+echo.
+echo.
